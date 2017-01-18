05676b8471323d398bdc6e1716810a85142eb20d
MozgiDev <marianbergerot@gmail.com>
2017-01-18 15:50:10 +0100

    v1

---
+++ client.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net"
+	"fmt"
+	"bufio"
+	"os"
+)
+
+var name string
+
+func main() {
+
+	fmt.Print("Nom: ")
+	fmt.Scanln(&name)
+	fmt.Println(name +": ")
+
+	conn, err := net.Dial("tcp", "localhost:4321")
+	if err != nil {
+	}
+	go SendMsg(conn)
+	for {
+		msg, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+		}
+		fmt.Print(msg)
+	}
+}
+
+func SendMsg(conn net.Conn) {
+	for  {
+		reader := bufio.NewReader(os.Stdin)
+		text, _ := reader.ReadString('\n')
+		fmt.Fprintf(conn, name +": "+ text)
+	}
+}
---
+++ server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net"
+	"fmt"
+	"bufio"
+)
+
+func main() {
+
+	ln, err := net.Listen("tcp", ":4321")
+	if err != nil {
+		// handle error
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			// handle error
+		}
+		go handleConnection(conn)
+	}
+}
+
+
+var Clients []*net.Conn
+func handleConnection(conn net.Conn) {
+	Clients = append(Clients, &conn)
+	for {
+		status, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+		}
+		go SendAll(status)
+	}
+}
+
+func SendAll(msg string) {
+	for _, client := range Clients {
+		fmt.Fprintf(*client, msg)
+	}
+}
